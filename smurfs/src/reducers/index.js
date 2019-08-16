c85856dfbd46a13bbabacf7bd8d571acac6778ca
masonkarsevar <masonkarsevar@gmail.com>
2019-08-16 08:25:58 -0700

    mock reducer and actions

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,33 @@
+import {
+    FETCH_CHARACTERS_START,
+    FETCH_CHARACTERS_SUCCESS,
+    FETCH_CHARACTERS_FAILURE,
+} from '../actions';
+
+const initialState = {
+    smurfs: []
+}
+
+function reducer(state=initialState, action) {
+    switch (action.type) {
+        case FETCH_CHARACTERS_START:
+            return {
+                ...state
+            }
+
+        case FETCH_CHARACTERS_SUCCESS:
+            return {
+                ...state
+            }
+
+        case FETCH_CHARACTERS_FAILURE:
+            return {
+                ...state
+            }
+
+        default:
+            return state
+    }
+}
+
+export default reducer;
